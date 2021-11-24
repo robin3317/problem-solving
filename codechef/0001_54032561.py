b5298efd362978367e6f688c7bd03fa7eb5a1766
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2021-11-25 00:08:26 +0600

    add two numbers

---
+++ codechef/0001_54032561.py
@@ -0,0 +1,11 @@
+#Note that it's python3 Code. Here, we are using input() instead of raw_input().
+#You can check on your local machine the version of python by typing "python --version" in the terminal.
+
+#Read the number of test cases.
+T = int(input())
+for tc in range(T):
+	# Read integers a and b.
+	(a, b) = map(int, input().split(' '))
+	
+	ans = a + b
+	print(ans)

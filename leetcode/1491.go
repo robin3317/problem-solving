546e76ec1e81f294b20df1f76faf7c9623638e7e
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-01 09:43:28 +0600

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ leetcode/1491.go
@@ -0,0 +1,37 @@
+/*
+1491. Average Salary Excluding the Minimum and Maximum Salary
+You are given an array of unique integers salary where salary[i] is the
+salary of the ith employee.
+Return the average salary of employees excluding the minimum and maximum salary.
+Answers within 10-5 of the actual answer will be accepted.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := average([]int{4000, 3000, 1000, 2000})
+	fmt.Println(result)
+}
+
+// leetcode part
+func average(salary []int) float64 {
+	minSalary := salary[0]
+	maxSalary := salary[1]
+	total := 0
+
+	for _, val := range salary {
+		if val < minSalary {
+			minSalary = val
+		} else if val > maxSalary {
+			maxSalary = val
+		}
+
+		total += val
+	}
+
+	return float64(total-minSalary-maxSalary) / float64(len(salary)-2)
+}
--- leetcode/1523.go
+++ leetcode/1523.go
@@ -1,9 +1,19 @@
 /*
 1523. Count Odd Numbers in an Interval Range
-Given two non-negative integers "low" and "high". 
+Given two non-negative integers "low" and "high".
 Return the count of odd numbers between "low" and "high" (inclusive).
 */
 
+package main
+
+import "fmt"
+
+func main() {
+	result := countOdds(3, 7)
+	fmt.Println(result)
+}
+
+// leetcode part
 func countOdds(low int, high int) int {
   count := 0
   for i := low; i <= high; i++ {

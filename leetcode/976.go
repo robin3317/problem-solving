ac0bcc2d31646146c86dad8891b24f547e2456ac
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-03 15:35:20 +0600

    976. Largest Perimeter Triangle

--- leetcode/976.go
+++ leetcode/976.go
@@ -23,8 +23,6 @@ func largestPerimeter(nums []int) int {
 	}
 	sort.Ints(nums)
 
-	//1, 3, 4, 8
-
 	for i := len(nums) - 1; i >= 2; i-- {
 		if nums[i-1]+nums[i-2] > nums[i] {
 			return nums[i] + nums[i-1] + nums[i-2]

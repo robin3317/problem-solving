3c84a83033be6b59a4bb634c7f4d4fdcbb1cd2c2
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-04 11:30:31 +0600

    1502. Can Make Arithmetic Progression From Sequence

--- leetcode/1502.go
+++ leetcode/1502.go
@@ -8,14 +8,24 @@ arithmetic progression. Otherwise, return false.
 */
 package main
 
-import "sort"
+import "fmt"
 
 func main() {
-	canMakeArithmeticProgression([]int{1, 2, 4})
+	fmt.Println(canMakeArithmeticProgression([]int{1, 2, 4}))
 }
 
 func canMakeArithmeticProgression(arr []int) bool {
-	sort.Ints(arr)
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+	// sort.Ints(arr)
 	diff := arr[0] - arr[1]
 
 	for i := 1; i < len(arr); i++ {

bdf0e6592f8b8a793d30aa146507538786f62f1c
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-04 09:37:01 +0600

    1523. Count Odd Numbers in an Interval Range

--- leetcode/1523.go
+++ leetcode/1523.go
@@ -15,11 +15,11 @@ func main() {
 
 // leetcode part
 func countOdds(low int, high int) int {
-  count := 0
-  for i := low; i <= high; i++ {
-    if i % 2 != 0 {
-      count++
-    }
-  }
-  return count
+	count := 0
+	for i := low; i <= high; i++ {
+		if i%2 != 0 {
+			count++
+		}
+	}
+	return count
 }

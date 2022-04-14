6e8892a60943fdbe29da9d4585de7199fec027a7
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:05:03 +0600

    1491. Average Salary Excluding the Minimum and Maximum Salary

--- leetcode/1491.go
+++ leetcode/1491.go
@@ -1,10 +1,4 @@
-/*
-1491. Average Salary Excluding the Minimum and Maximum Salary
-You are given an array of unique integers salary where salary[i] is the
-salary of the ith employee.
-Return the average salary of employees excluding the minimum and maximum salary.
-Answers within 10-5 of the actual answer will be accepted.
-*/
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
 
 package main
 

486fe0fe2a212265596d60d5bb3c178d0bc04e6e
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:07:55 +0600

    1572. Matrix Diagonal Sum

--- leetcode/1572.go
+++ leetcode/1572.go
@@ -1,11 +1,4 @@
-/*
-1572. Matrix Diagonal Sum
-
-Given a square matrix mat, return the sum of the matrix diagonals.
-
-Only include the sum of all the elements on the primary diagonal and all the elements
-on the secondary diagonal that are not part of the primary diagonal.
-*/
+// https://leetcode.com/problems/matrix-diagonal-sum/
 
 package main
 

6d9c3d60dbf5a552725951fac53bb3c5096e1447
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:01:04 +0600

    191. Number of 1 Bits

--- leetcode/0191.go
+++ leetcode/0191.go
@@ -1,8 +1,4 @@
-/*
-191. Number of 1 Bits
-Write a function that takes an unsigned integer and returns the number of '1' bits
-it has (also known as the Hamming weight).
-*/
+// https://leetcode.com/problems/number-of-1-bits/
 
 package main
 

a116ecd5c77cb196cfdbd9b131ba8bb7624f0b5b
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:09:27 +0600

    1822. Sign of the Product of an Array

--- leetcode/1822.go
+++ leetcode/1822.go
@@ -1,14 +1,5 @@
-/*
-1822. Sign of the Product of an Array
-There is a function signFunc(x) that returns:
+// https://leetcode.com/problems/sign-of-the-product-of-an-array/
 
-1 if x is positive.
--1 if x is negative.
-0 if x is equal to 0.
-You are given an integer array nums. Let product be the product of all values in the array nums.
-
-Return signFunc(product).
-*/
 package main
 
 func main() {

62a4de736b1c008066f2f80396941feb8ff137da
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:04:07 +0600

    1281. Subtract the Product and Sum of Digits of an Integer

--- leetcode/1281.go
+++ leetcode/1281.go
@@ -1,8 +1,4 @@
-/*
-1281. Subtract the Product and Sum of Digits of an Integer
-Given an integer number n, return the difference between the product of its
-digits and the sum of its digits.
-*/
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 
 package main
 

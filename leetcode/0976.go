9d5dc6c47dd251c7c703943bc822e25325ac7b14
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:02:52 +0600

    https://leetcode.com/problems/largest-perimeter-triangle/

--- leetcode/0976.go
+++ leetcode/0976.go
@@ -1,9 +1,5 @@
-/*
-976. Largest Perimeter Triangle
-Given an integer array nums, return the largest perimeter of a triangle
-with a non-zero area, formed from three of these lengths.
-If it is impossible to form any triangle of a non-zero area, return 0.
-*/
+// https://leetcode.com/problems/largest-perimeter-triangle/
+
 package main
 
 import (

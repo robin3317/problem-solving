ebc20b423ad4d8974dda7cfd2cd62bc3dbca17ab
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-04-14 09:06:03 +0600

    1502. Can Make Arithmetic Progression From Sequence

--- leetcode/1502.go
+++ leetcode/1502.go
@@ -1,11 +1,5 @@
-/*
-1502. Can Make Arithmetic Progression From Sequence
-A sequence of numbers is called an arithmetic progression if the difference between
-any two consecutive elements is the same.
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
 
-Given an array of numbers arr, return true if the array can be rearranged to form an
-arithmetic progression. Otherwise, return false.
-*/
 package main
 
 import "fmt"

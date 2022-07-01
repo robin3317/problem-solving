2482e93a2f5955416c2a2666f7bb328ab8c32470
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-07-02 02:21:58 +0600

    876. Middle of the Linked List

---
+++ leetcode/0876.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};

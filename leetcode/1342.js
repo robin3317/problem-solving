79d24e3b56548c624764c523f7664b6c0a6cdd34
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2022-07-01 18:03:22 +0600

    1342. Number of Steps to Reduce a Number to Zero

---
+++ leetcode/1342.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+var numberOfSteps = function (num) {
+  let steps = 0;
+  while (num !== 0) {
+    if ((num & 1) === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    steps++;
+  }
+  return steps;
+};

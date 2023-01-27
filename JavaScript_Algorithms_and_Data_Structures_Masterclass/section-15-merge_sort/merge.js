c1762936c8151f15c6ce94a3ab04e2832ee9e03d
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-01-27 15:14:38 +0600

    merge sort

---
+++ JavaScript_Algorithms_and_Data_Structures_Masterclass/section-15-merge_sort/merge.js
@@ -0,0 +1,38 @@
+function merge(arr1, arr2) {
+  const result = []
+  let i = 0,
+    j = 0
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i])
+      i++
+    } else {
+      result.push(arr2[j])
+      j++
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i])
+    i++
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j])
+    j++
+  }
+
+  return result
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+  const mid = Math.floor(arr.length / 2)
+  const left = mergeSort(arr.slice(0, mid))
+  const right = mergeSort(arr.slice(mid))
+  return merge(left, right)
+}
+
+const result = mergeSort([1, 5, 8, 2, 6, 7])
+console.log(result)

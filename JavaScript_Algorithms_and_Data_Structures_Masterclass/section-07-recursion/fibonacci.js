6290b1191b700ae7de3129ba2ea92aed70b2771e
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-01-28 16:09:38 +0600

    correct fibonaaci logic

--- JavaScript_Algorithms_and_Data_Structures_Masterclass/section-07-recursion/fibonacci.js
+++ JavaScript_Algorithms_and_Data_Structures_Masterclass/section-07-recursion/fibonacci.js
@@ -1,16 +1,16 @@
 // program to display fibonacci sequence using recursion
 function fibonacci(num) {
-  if (num < 2) {
-    return num
+  if (num <= 2) {
+    return num - 1
   }
   return fibonacci(num - 1) + fibonacci(num - 2)
-  // fib(5)                                                                + fib(4)
-  // fib(4)                                     + fib(3)                   + fib(3)                   + fib(2)
-  // fib(3)                   + fib(2)          + fib(2)          + fib(1) + fib(2)          + fib(1) + fib(1) + fib(0)
-  // fib(2)          + fib(1) + fib(1) + fib(0) + fib(1) + fib(0) + 1      + fib(1) + fib(0) + 1      + 1      + 0
-  // fib(1) + fib(0) + 1      + 1      + 0      + 1      + 0      + 1       + 1     + 0      + 1      + 1      + 0
-  // 1 + 0 + 1 + 1 + 0 + 1 + 0 + 1 + 1 + 0 + 1 + 1 + 0
-  // 8
+  // fibonacci(6)
+  // fib(5)                                     + fib(4)
+  // fib(4)                   + fib(3)          + fib(3)          + fib(2)
+  // fib(3)          + fib(2) + fib(2) + fib(1) + fib(2) + fib(1) + 1
+  // fib(2) + fib(1) + 1      + 1      + 0      + 1      + 0      + 1
+  // 1      + 0      + 1      + 1      + 0      + 1      + 0      + 1
+  // 5
 }
 
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144

0a301591a9b62069c0fda95fb3051603d92ebe7b
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-01-28 22:13:12 +0600

    is valid subsequence

---
+++ AlgoExpert/isValidSubsequence.js
@@ -0,0 +1,23 @@
+/*
+  Given two non-empty arrays of integers, write a function that determines
+  whether the second array is a subsequence of the first one.
+
+  A subsequence of an array is a set of numbers that aren't necessarily adjacent
+  in the array but that are in the same order as they appear in the array. For
+  instance, the numbers [1, 3, 4] form a subsequence of the array
+  [1, 2, 3, 4], and so do the numbers [2, 4]. Note
+  that a single number in an array and the array itself are both valid
+  subsequences of the array.
+*/
+function isValidSubsequence(array, sequence) {
+  let seqIdx = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === sequence[seqIdx]) {
+      seqIdx++
+    }
+  }
+  return seqIdx === sequence.length
+}
+
+const result = isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])
+console.log(result)

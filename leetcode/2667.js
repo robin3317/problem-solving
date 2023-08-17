cd2e293dda09963b938ad3ce4034767b98d57b1f
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-08-17 22:25:32 +0600

    2667. Create Hello World Function

---
+++ leetcode/2667.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/create-hello-world-function
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+  return function (...args) {
+    return 'Hello World'
+  }
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

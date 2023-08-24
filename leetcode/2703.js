686f38ca6f664c79a73207da2c5e6b258c85f546
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-08-24 11:52:00 +0600

    2703. Return Length of Arguments Passed

---
+++ leetcode/2703.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */

59724bfd6218e61e123132169a14a4ef54eec7fc
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2023-08-28 01:17:46 +0600

    2623. Memoize

---
+++ leetcode/2623.js
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/memoize
+
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const map = new Map()
+    return function(...args) {
+        const key = JSON.stringify(args)
+        if (map.has(key)) {
+            return map.get(key)
+        }
+        const result = fn(...args)
+        map.set(key, result)
+        return result
+    }
+}
+// function memoize(fn) {
+//     const cache = {}
+//     return function(...args) {
+//         const key = JSON.stringify(args)
+//         if (key in cache) {
+//             return cache[key]
+//         }
+//         const result = fn(...args)
+//         cache[key] = result
+//         return result
+//     }
+// }
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
